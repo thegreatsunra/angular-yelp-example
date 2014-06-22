7d6d9ddeb6da4e1df16fa01aab54a67455b4fdd6
Dane Petersen <thegreatsunra@gmail.com>
2014-06-21 22:38:43 -0700

    Add JavaScript example code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,4 +4,40 @@
 
 'use strict';
 
-console.log('\'Allo \'Allo!');
+angular.module('yelpApp', [])
+  .controller('HomeController', function($scope, $http) {
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/' +
+                     'forecast/daily?mode=json' +
+                     '&units=imperial&cnt=1' +
+                     '&callback=JSON_CALLBACK' +
+                     '&q=';
+    $scope.getWeather = function() {
+      $http({
+        method: 'JSONP',
+        url: weatherUrl + $scope.city
+      }).then(function(data) {
+        console.log(data);
+        $scope.weather = data.data;
+      });
+
+      $scope.getLocalBusinesses();
+    };
+
+    var yelpUrl = 'http://api.yelp.com/business_review_search',
+        yelpKey = '**PAST YOUR YELP API KEY HERE**';
+
+    $scope.getLocalBusinesses = function() {
+      $http({
+        method: 'JSONP',
+        url: yelpUrl,
+        params: {
+          callback: 'JSON_CALLBACK',
+          ywsid: yelpKey,
+          location: $scope.city
+        }
+      }).then(function(data) {
+        console.log(data);
+        $scope.nearby = data.data.businesses;
+      });
+    };
+  });
